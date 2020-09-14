dd8b39faecb586d8625716c31f6c3c0378612e1b
Kyohei Kondo <plasma.reconstruction.mccs@gmail.com>
2020-09-14 18:15:18 +0900

    added api

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -7,7 +7,11 @@ const awsmobile = {
     "aws_cognito_region": "ap-northeast-1",
     "aws_user_pools_id": "ap-northeast-1_Qx4t21cDf",
     "aws_user_pools_web_client_id": "7e740msrt8s5bf26lot7vlgu5u",
-    "oauth": {}
+    "oauth": {},
+    "aws_appsync_graphqlEndpoint": "https://q355ahwwgzb5dnlfwzzfcob2ne.appsync-api.ap-northeast-1.amazonaws.com/graphql",
+    "aws_appsync_region": "ap-northeast-1",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": "da2-erpgj3th2fetrihuk2t25x3kqa"
 };
 
 
