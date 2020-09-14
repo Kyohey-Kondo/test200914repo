4d292a7cef8a153e8939ca8e1670ad80264ffd51
Kyohei Kondo <plasma.reconstruction.mccs@gmail.com>
2020-09-14 17:31:15 +0900

    code in index.js is modified as awsmobile -> config

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 import Amplify from 'aws-amplify';
-import awsmobile from './aws-exports';
+import config from './aws-exports';
 
-Amplify.configure(awsmobile);
+Amplify.configure(config);
 
 ReactDOM.render(
   <React.StrictMode>
